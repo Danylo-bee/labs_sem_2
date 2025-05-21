8945bdf2a54b913bfec482d3ecc4472d59c45957
Danylo-bee <61242093+Danylo-bee@users.noreply.github.com>
2025-05-21 18:49:41 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,75 @@
+import unittest
+from ilands import find_islands
+
+class TestFindIslands(unittest.TestCase):
+
+    def test_single_island(self):
+        matrix = [
+            [1, 1, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]
+        self.assertEqual(find_islands(matrix), 2)
+
+    def test_multiple_islands(self):
+        matrix = [
+            [1, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0],
+            [1, 1, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0]
+        ]
+        self.assertEqual(find_islands(matrix), 2)
+
+    def test_no_island(self):
+        matrix = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]
+        self.assertEqual(find_islands(matrix), 0)
+
+    def test_full_island(self):
+        matrix = [
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1]
+        ]
+        self.assertEqual(find_islands(matrix), 1)
+
+    def test_single_cell_island(self):
+        matrix = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]
+        self.assertEqual(find_islands(matrix), 1)
+
+    def test_large_island(self):
+        matrix = [
+            [1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        self.assertEqual(find_islands(matrix), 1)
+
+if __name__ == '__main__':
+    unittest.main()
